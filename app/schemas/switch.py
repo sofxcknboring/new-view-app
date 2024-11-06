aa21dcce4932618cb7f81c7f4c80f8916d7c89ea
sofxcknboring <paranoiawe@gmail.com>
2024-11-07 01:05:03 +0300

    upd switch schemas

--- app/schemas/switch.py
+++ app/schemas/switch.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 
 from pydantic import BaseModel, Field, field_validator
 
-from .device import DeviceRead
+from .device import DeviceRead, DeviceResponse
 from .validation_helper import validation_helper
 
 
@@ -10,31 +10,26 @@ class SwitchBase(BaseModel):
     comment: Optional[str] = Field(None, max_length=50, description="Комментарий. Не более 50 символов")
 
 
+class SwitchIpAddress(BaseModel):
+    ip_address: Optional[str] = Field(description="IP-адрес коммутатора")
+
+    @field_validator("ip_address")
+    def validate_mac_address(cls, ip_address):
+        ip_address = validation_helper.validate_ip_address(ip_address)
+        return ip_address
+
+
 class SwitchCreate(SwitchBase):
-    ip_address: Optional[str] = Field(None, description="IP-адрес коммутатора")
+    ip_address: SwitchIpAddress = Field(None, description="IP-адрес коммутатора")
     snmp_oid: Optional[str] = Field("1.3.6.1.2.1.17.7.1.2.2.1.2", description="Идентификатор SNMP-агента")
     core_switch_ip: str = Field(None, description="IP-адрес опорного коммутатора")
     excluded_ports_relation: Optional[List[int]] = Field(
         [23, 24, 25, 26, 27, 28, 29, 30, 105, 209, 1000], description="Excluded ports"
     )
 
-    @field_validator("ip_address", "core_switch_ip")
-    @classmethod
-    def validate_ip_address(cls, value: Optional[str]) -> Optional[str]:
-        return validation_helper.validate_ip_address(ip=value)
-
 
 class SwitchUpdate(SwitchCreate):
     pass
-
-
-class SwitchIpAddress(BaseModel):
-    ip_address: Optional[str] = Field(description="IP-адрес коммутатора")
-
-    @field_validator("ip_address")
-    @classmethod
-    def validate_ip_address(cls, value: Optional[str]) -> Optional[str]:
-        return validation_helper.validate_ip_address(ip=value)
 
 
 class ExcludedPortBase(BaseModel):
@@ -50,8 +45,14 @@ class SwitchExcludedPortBase(BaseModel):
 
 class SwitchRead(SwitchBase):
     id: int
-    ip_address: str
+    ip_address: SwitchIpAddress
     snmp_oid: str
     core_switch_ip: str
     devices: Optional[List[DeviceRead]] = []
     excluded_ports_relation: List[SwitchExcludedPortBase] = []
+
+
+class SwitchResponse(SwitchBase):
+    ip_address: str
+    core_switch_ip: str
+    devices: Optional[List[DeviceResponse]] = []
