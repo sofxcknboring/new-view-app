332af9757e792dbfc29fd0bd8887ea64119de161
sofxcknboring <paranoiawe@gmail.com>
2024-11-08 12:58:07 +0300

    Add CORS

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import uvicorn
 from core.config import settings
 from core.models import db_helper
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 description = """
 Net-View API. 🚀
@@ -67,6 +67,18 @@ main_app = FastAPI(
 )
 main_app.include_router(api.router)
 
+
+origins = [
+    "https://grafana.danilenko.tech",
+]
+
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:main_app", host=settings.run.host, port=settings.run.port, reload=True, workers=4)
