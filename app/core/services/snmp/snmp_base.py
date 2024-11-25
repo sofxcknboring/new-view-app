9819b28fdd34c584dacd49ffecf751d94098600f
sofxcknboring <paranoiawe@gmail.com>
2024-11-25 17:46:15 +0300

    upd snmp_base

--- app/core/services/snmp/snmp_base.py
+++ app/core/services/snmp/snmp_base.py
@@ -15,5 +15,21 @@ class SnmpBase(ABC):
         pass
 
     @abstractmethod
-    async def walk_all(self, method_name):
+    async def get_sys_descr(self, target_ip, current_oid):
+        pass
+
+    @abstractmethod
+    async def get_port_vlan_table(self, target_ip, current_oid):
+        pass
+
+    @abstractmethod
+    async def get_mac_vlan_port_mappings(self, target_ip, ports, snmp_oid):
+        pass
+
+    @abstractmethod
+    async def get_arp_table(self, target_ip, snmp_oid):
+        pass
+
+    @abstractmethod
+    async def walk_all(self, method_name, target_switches):
         pass
